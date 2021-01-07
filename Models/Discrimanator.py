b408aee3386a3fa4f1dd9c6b4c35229da253c33f
Daniel Roich <danielroich@gmail.com>
2021-01-07 11:51:00 +0200

    Changing Discriminator and MLP
    In order to resemble Yotam's networks

--- Models/Discrimanator.py
+++ Models/Discrimanator.py
@@ -4,22 +4,24 @@ import torch.nn as nn
 class Discriminator(nn.Module):
     def __init__(self, num_features, n_hid, dropout = 0.25):
         super(Discriminator, self).__init__()
-        self.main = nn.Sequential(
+        slope = 0.2
+        self.sequential = nn.Sequential(
             nn.Linear(num_features, n_hid),
-            nn.LeakyReLU(),
-            nn.BatchNorm1d(n_hid, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-            nn.Dropout(dropout),
+            nn.LeakyReLU(negative_slope=slope),
+            # nn.BatchNorm1d(n_hid, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+            # nn.Dropout(dropout),
             nn.Linear(n_hid, n_hid // 2),
-            nn.LeakyReLU(),
-            nn.BatchNorm1d(n_hid // 2, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-            nn.Dropout(dropout),
+            nn.LeakyReLU(negative_slope=slope),
+            # nn.BatchNorm1d(n_hid // 2, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+            # nn.Dropout(dropout),
             nn.Linear(n_hid // 2, n_hid // 4),
-            nn.LeakyReLU(),
-            nn.BatchNorm1d(n_hid // 4, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-            nn.Dropout(dropout),
+            nn.LeakyReLU(negative_slope=slope),
+            # nn.BatchNorm1d(n_hid // 4, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+            # nn.Dropout(dropout),
             nn.Linear(n_hid // 4, 1),
-            nn.Sigmoid()
+            # nn.Sigmoid()
         )
+        self.main = self.sequential
 
     def forward(self, input_data):
         return self.main(input_data)
--- Models/MLP.py
+++ Models/MLP.py
@@ -4,19 +4,20 @@ import torch.nn as nn
 class MLPModel(nn.Module):
     def __init__(self, num_features, dropout=0.25, n_hid=128):
         super().__init__()
+        slope = 0.2
         self.model = nn.Sequential(
             nn.Linear(num_features, n_hid),
-            nn.LeakyReLU(),
-            nn.BatchNorm1d(n_hid, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-            nn.Dropout(dropout),
+            nn.LeakyReLU(negative_slope=slope),
+            # nn.BatchNorm1d(n_hid, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+            # nn.Dropout(dropout),
             nn.Linear(n_hid, n_hid // 2),
-            nn.LeakyReLU(),
-            nn.BatchNorm1d(n_hid // 2, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-            nn.Dropout(dropout),
+            nn.LeakyReLU(negative_slope=slope),
+            # nn.BatchNorm1d(n_hid // 2, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+            # nn.Dropout(dropout),
             nn.Linear(n_hid // 2, n_hid // 4),
-            nn.LeakyReLU(),
-            nn.BatchNorm1d(n_hid // 4, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-            nn.Dropout(dropout),
+            nn.LeakyReLU(negative_slope=slope),
+            # nn.BatchNorm1d(n_hid // 4, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+            # nn.Dropout(dropout),
             nn.Linear(n_hid // 4, 512)
         )
         for m in self.model:
