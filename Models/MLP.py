dfcc504f33c109dc4450b6dfe3ade70adc24dff5
Daniel Roich <danielroich@gmail.com>
2021-01-07 12:25:41 +0200

    fix nets

--- Models/MLP.py
+++ Models/MLP.py
@@ -8,16 +8,13 @@ class MLPModel(nn.Module):
         self.model = nn.Sequential(
             nn.Linear(num_features, n_hid),
             nn.LeakyReLU(negative_slope=slope),
-            # nn.BatchNorm1d(n_hid, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-            # nn.Dropout(dropout),
+            nn.BatchNorm1d(n_hid, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.Linear(n_hid, n_hid // 2),
             nn.LeakyReLU(negative_slope=slope),
-            # nn.BatchNorm1d(n_hid // 2, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-            # nn.Dropout(dropout),
+            nn.BatchNorm1d(n_hid // 2, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.Linear(n_hid // 2, n_hid // 4),
             nn.LeakyReLU(negative_slope=slope),
-            # nn.BatchNorm1d(n_hid // 4, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-            # nn.Dropout(dropout),
+            nn.BatchNorm1d(n_hid // 4, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.Linear(n_hid // 4, 512)
         )
         for m in self.model:
