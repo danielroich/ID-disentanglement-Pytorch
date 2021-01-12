a8c73401155bd31c2abbe1375b9180f516e5776b
Daniel Roich <danielroich@gmail.com>
2021-01-12 18:09:46 +0200

    fix l1 loss and adding mapper training

--- Losses/NonAdversarialLoss.py
+++ Losses/NonAdversarialLoss.py
@@ -2,12 +2,8 @@ from pytorch_msssim import MS_SSIM
 import numpy as np
 import torch
 
-def is_same_image(image1, image2):
-    return image1.shape == image2.shape and not(np.bitwise_xor(image1,image2).any())
-
-
 def reconstruction_loss(Iattr, Iout,Iid, a):
-  if is_same_image(Iattr, Iid):
+
     img_attr = torch.from_numpy(np.rollaxis(Iattr, 2)).float().unsqueeze(0)/255.0
     if torch.cuda.is_available():
       img_attr = img_attr.cuda()
@@ -18,11 +14,11 @@ def reconstruction_loss(Iattr, Iout,Iid, a):
     ms_ssim_val = ms_ssim( img_attr, img_out, data_range=255, size_average=False )
     norm1=np.linalg.norm(np.asarray(Iattr)-np.asarray(Iout))
     return a*(1-ms_ssim_val)+(1-a)*norm1
-  else:
-    return 0
+
 
 def id_loss(encoded_input_image, encoded_generated_image):
-    return torch.nn.L1Loss(encoded_input_image, encoded_generated_image)
+    loss = torch.nn.L1Loss()
+    return loss(encoded_input_image, encoded_generated_image)
 
 def landmark_loss(input_attr_lnd, output_lnd):
     loss = torch.norm(input_attr_lnd - output_lnd, p=2)
