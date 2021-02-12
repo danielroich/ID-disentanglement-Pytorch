21442ea243fb89859c657743c568749efcae804e
nofarm <nofarm96@gmail.com>
2021-02-12 15:57:01 +0200

    add meta to E_attr

--- Models/Encoders/Attribute_Encoder.py
+++ Models/Encoders/Attribute_Encoder.py
@@ -10,6 +10,9 @@ class Encoder_Attribute(nn.Module):
         super(Encoder_Attribute, self).__init__()
         self.model = torch.hub.load('pytorch/vision:v0.6.0', 'inception_v3', pretrained=True, aux_logits=False)
         self.model.fc = Identity()
+        self.meta = {'mean': [0.485, 0.456, 0.406],
+                     'std': [0.229, 0.224, 0.225],
+                     'imageSize': [299, 299, 3]}
 
     def forward(self, data):
         return self.model(data)
