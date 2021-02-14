51bcd223e951c23680dce0489af7ae1dcc63c8e0
danielroich <danielroich@gmail.com>
2021-02-14 16:12:21 +0200

    adding landmark loss for training + testing landmark

--- Training/trainer.py
+++ Training/trainer.py
@@ -13,6 +13,7 @@ class Trainer:
                  generator,
                  id_transform,
                  attr_transform,
+                 landmark_transform,
                  id_encoder,
                  attr_encoder,
                  landmark_encoder):
@@ -25,6 +26,7 @@ class Trainer:
         self.generator = generator
         self.id_transform = id_transform
         self.attr_transform = attr_transform
+        self.landmark_transform = landmark_transform
         self.id_encoder = id_encoder
         self.attr_encoder = attr_encoder
         self.landmark_encoder = landmark_encoder
@@ -64,7 +66,7 @@ class Trainer:
         # Return error
         return error, prediction
 
-    def discriminator_train_step(self, real_w, fake_data, print_results=True):
+    def adversarial_train_step(self, real_w, fake_data, print_results=True):
         error_real, prediction_real, error_fake, prediction_fake = self.train_discriminator(real_w, fake_data)
         g_error, g_pred = self.train_mapper(fake_data)
 
@@ -78,9 +80,9 @@ class Trainer:
 
             return error_real, error_fake, prediction_real, prediction_fake, g_error, g_pred
 
-    def id_and_rec_train_step(self, fake_data,
-                               original_id_vec, original_attr_images,
-                               are_the_same_images=True, print_results=True):
+    def non_adversarial_train_step(self, fake_data,
+                                   original_id_vec, original_attr_images,
+                                   are_the_same_images=True, print_results=True):
 
         self.optimizer_M.zero_grad()
 
@@ -90,27 +92,39 @@ class Trainer:
         generated_images = (generated_images + 1) / 2
 
         id_generated_images = self.id_transform(generated_images)
-        #attr_generated_images = self.attr_transform(generated_images)
 
         pred_id_embedding = torch.squeeze(self.id_encoder(id_generated_images))
         id_loss_val = self.config['lambdaID'] * id_loss(original_id_vec, pred_id_embedding)
-        #
-        # _, generated_landmarks = self.landmark_encoder(attr_generated_images.cpu().detach().numpy())
-        # _, real_landmarks = self.landmark_encoder(original_attr_images)
-        # landmark_loss_val = self.config['lambdaLND'] * landmark_loss(generated_landmarks, real_landmarks)
+
+        landmark_attr_images = self.landmark_transform(original_attr_images).permute(0, 2, 3, 1) \
+                                    .cpu().numpy() * 255
+        landmark_generated_images = self.landmark_transform(generated_images.detach()) \
+                                         .permute(0, 2, 3, 1).cpu().numpy() * 255
+
+        try:
+            _, generated_landmarks = self.landmark_encoder(landmark_generated_images)
+            _, real_landmarks = self.landmark_encoder(landmark_attr_images)
+            landmark_loss_val = self.config['lambdaLND'] * landmark_loss(generated_landmarks, real_landmarks)
+
+        except Exception as e:
+            landmark_loss_val = 0
+            if print_results:
+                print(str(e))
 
         if are_the_same_images:
-            rec_loss_val = self.config['lambdaREC'] * rec_loss(original_attr_images, generated_images, self.config['a'])
+            rec_loss_val = self.config['lambdaREC'] * rec_loss(original_attr_images,
+                                                               generated_images, self.config['a'])
         else:
             rec_loss_val = 0
 
-        total_error = rec_loss_val + id_loss_val
+        total_error = rec_loss_val + id_loss_val + landmark_loss_val
 
         total_error.backward()
         self.optimizer_M.step()
 
         if print_results:
             print(f"id_loss_val: {id_loss_val}")
+            print(f"landmark_loss: {landmark_loss_val}")
             print(f"rec_loss: {rec_loss_val}")
 
         return id_loss_val, rec_loss_val
