8e057fc6ed5e859bad077f17469292b69324c560
danielroich <danielroich@gmail.com>
2021-02-27 14:56:28 +0200

    add division by batch size on landmark loss

--- Losses/NonAdversarialLoss.py
+++ Losses/NonAdversarialLoss.py
@@ -1,8 +1,6 @@
 from pytorch_msssim import ms_ssim
 import torch
 
-# TODO: divide losses by batch sizes?
-
 def rec_loss(attr_images, generated_images, a):
     ms_ssim_loss = 1 - ms_ssim(attr_images, generated_images, data_range=1, size_average=True)
     l1_loss_value = torch.norm(attr_images - generated_images, p=1)
@@ -14,5 +12,5 @@ def id_loss(encoded_input_image, encoded_generated_image):
 
 
 def landmark_loss(input_attr_lnd, output_lnd):
-    loss = torch.norm(input_attr_lnd - output_lnd, p=2)
+    loss = torch.norm(input_attr_lnd - output_lnd, p=2) / input_attr_lnd.size()[0]
     return loss
