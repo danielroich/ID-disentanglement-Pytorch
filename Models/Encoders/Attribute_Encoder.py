3adaf9b85a8c49c7367e8529a013a5e619171e3b
nofarm <nofarm96@gmail.com>
2021-03-01 23:39:27 +0200

    pretrained as an arg for Encoder_Attribute

--- Models/Encoders/Attribute_Encoder.py
+++ Models/Encoders/Attribute_Encoder.py
@@ -6,9 +6,9 @@ import torchvision.transforms as transforms
 
 
 class Encoder_Attribute(nn.Module):
-    def __init__(self):
+    def __init__(self,  pretrained=False):
         super(Encoder_Attribute, self).__init__()
-        self.model = torch.hub.load('pytorch/vision:v0.6.0', 'inception_v3', pretrained=True, aux_logits=False)
+        self.model = torch.hub.load('pytorch/vision:v0.6.0', 'inception_v3', pretrained=pretrained, aux_logits=False, init_weights=not pretrained)
         self.model.fc = Identity()
         self.meta = {'mean': [0.485, 0.456, 0.406],
                      'std': [0.229, 0.224, 0.225],
